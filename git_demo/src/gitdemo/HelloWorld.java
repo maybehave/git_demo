a68acfeb9f68623f1546902722a2b7580be7b5a7
gang <not_twenty@live.cn>
2019-05-28 17:10:43 +0800

    Ali

--- git_demo/src/gitdemo/HelloWorld.java
+++ git_demo/src/gitdemo/HelloWorld.java
@@ -1,9 +1,16 @@
-package git_demo;
+package gitdemo;
 
+
+/**
+ * @author Zn
+ *
+ * 2019年5月28日-下午5:09:10
+ */
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("NaNa,Where are you");
 	}
 
