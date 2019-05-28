a936e1ea5331331bb3d226bc40758713d24b5a4e
maybehave <44522292+maybehave@users.noreply.github.com>
2019-05-28 16:23:39 +0800

    Update HelloWorld.java
    第二次修改

--- git_demo/src/git_demo/HelloWorld.java
+++ git_demo/src/git_demo/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("NaNa");
+		System.out.println("NaNa,Where are you");
 	}
 
 }
